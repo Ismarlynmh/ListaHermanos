f0e1dbef1bb870988aa31c40c6f16c5f307a9894
Ismarlynmh <ismarlyn123@gmail.com>
2022-07-09 18:54:26 -0300

    Cambios actualizados

--- DAL/Contexto.cs
+++ DAL/Contexto.cs
@@ -7,8 +7,12 @@ namespace HermanosApi.DAL
     {
         public DbSet<Hermanos> Hermanos { get; set;}
 
-        public Contexto(DbContextOptions<Contexto> options) : base(options){
-        }
+        #pragma warning disable CS8618 // Un campo que no acepta valores NULL debe contener un valor distinto de NULL al salir del constructor. Considere la posibilidad de declararlo como que admite un valor NULL.
+    public Contexto(DbContextOptions<Contexto> options) : base(options)
+#pragma warning restore CS8618 // Un campo que no acepta valores NULL debe contener un valor distinto de NULL al salir del constructor. Considere la posibilidad de declararlo como que admite un valor NULL.
+    {
+
+    }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder){
         
